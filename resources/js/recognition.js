916f9bad761707b3fb74e90da720e60641b2c5b0
Aman Sharma <amansharmash238@gmail.com>
2021-05-23 15:19:07 +0530

    Added main.css file, changed the recognition and synthesis.js file

--- resources/js/recognition.js
+++ resources/js/recognition.js
@@ -2,22 +2,28 @@ window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecogn
 const recognition = new SpeechRecognition()
 
 
+window.addEventListener("load", function loading() {
+    let started = document.getElementById("welcome");
+    // console.log(started.innerText);
+    p = started.innerText;
+    say(p)
+});
+
 function start_recognition() {
     recognition.start()
     console.log('Recognition started')
 }
 
-
 recognition.onresult = (event) => {
     const query = event.results[0][0].transcript
     console.log(query)
-    processQuery(query)
-}
-
-
-recognition.onnomatch = () => {
-    text = 'I didnt recognize that.'
-    say(text)
+    if (event.results[0][0].confidence > 0.3)
+        processQuery(query)
+    else {
+        text = 'I didnt recognize this.please try again';
+        console.log(text);
+        say(text)
+    }
 }
 
 
--- resources/js/synthesis.js
+++ resources/js/synthesis.js
@@ -2,6 +2,6 @@ const synth = window.speechSynthesis
 
 function say(text) {
     const utterance = new SpeechSynthesisUtterance(text)
-    utterance.rate = 1.5
+    utterance.rate = 1.0
     synth.speak(utterance)
 }
